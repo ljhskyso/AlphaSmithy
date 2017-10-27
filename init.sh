6a8cf898122f73df4ed81f67db15cf7c763d3aec
Jiheng Lu <jlu@groupon.com>
2017-10-26 23:00:48 -0700

    init firebase-function

--- init.sh
+++ init.sh
@@ -2,4 +2,5 @@
 
 git clone https://github.com/ljhskyso/AlphaSmithy-server.git
 git clone https://github.com/ljhskyso/AlphaSmithy-worker.git
+git clone https://github.com/ljhskyso/AlphaSmithy-firebase-functions.git
 docker-compose -f AlphaSmithy-server/docker-compose.yml up
